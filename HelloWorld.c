5aec998adb057a3e01e1ed527f007eeda240e112
XarCraft <batu.erinckan1@gmail.com>
2024-01-05 21:44:17 +0300

    Escape Sequences

--- HelloWorld.c
+++ HelloWorld.c
@@ -11,6 +11,6 @@ int main() {
 	// This Is a Normal Comment
 
 	puts("Welcome to My Program!\n\tHello From HelloWorld.c!\n"); // Used Only to Print Strings
-	printf("Hello, World!\n"); // Can Be Used For Printing Integer, Float, Character, Hexa Decimal Values and Strings
+	printf("Printing \"Hello, World!\"\t-\tSuccess!\n"); // Can Be Used For Printing Integer, Float, Character, Hexa Decimal Values and Strings
 	return 0;
 }
