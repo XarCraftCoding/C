e02a77d4109b0e45f8bcc69d6df17bad35db6f7e
XarCraft <batu.erinckan1@gmail.com>
2024-01-05 21:39:57 +0300

    Comments

--- HelloWorld.c
+++ HelloWorld.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
 
 int main() {
-	printf("Hello, World!\n");
+	/*
+		This Is A Multiline Comment
+	*/
+
+	// This Is An \
+	Extended Comment
+
+	// This Is a Normal Comment
+
+	puts("Welcome to My Program!\n\tHello From HelloWorld.c!\n"); // Used Only to Print Strings
+	printf("Hello, World!\n"); // Can Be Used For Printing Integer, Float, Character, Hexa Decimal Values and Strings
 	return 0;
 }
